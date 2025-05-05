7f97c1e57b43b5333ebf62c0be3bade342963e26
fiza-fiaz <Fiza.Fiaz@Student.HTW-Berlin.de>
2025-05-05 13:44:10 +0200

    Renew M1

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // importieren!
-
-const app = createApp(App);
-
-app.use(router); // Router verwenden
-app.mount('#app');
